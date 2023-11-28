89405439707f4ec662257eca664d2c1bec25f7ac
ZuzannaZimmer <225158@uek.student.krakow.pl>
2023-11-28 14:46:34 +0100

    zajecia

---
+++ 07-AggregationAndComposition/Folder.java
@@ -0,0 +1,47 @@
+public class Folder {
+    private int size;
+    private String nameFolder;
+    private String address;
+    private String created;
+    private File file;
+
+    public Folder(String name){
+        this.nameFolder=name;
+    }
+
+    // public void addFile(String name){
+    //     file = new File(name);
+      
+    // }
+        public void addFile(File file){
+        this.file = file;
+      
+    }
+
+    public void deleteFile(){
+        file = null;
+    }
+    public void setName(String name){
+        this.nameFolder = name;
+
+    }
+    public void changeAddress(String address){
+        this.address = address;
+
+    }
+    public void deleteFolder(String name){
+        this.nameFolder = null;
+    }
+    public void addFolder(String name){
+
+    }
+
+    @Override
+    public String toString() {
+        String s = "Nazwa folderu: "+ nameFolder;
+        s+=" Nazwa pliku: "+ file.getName();
+        return s;
+    }
+
+    
+}
